db29104ee2f1c7aeab6faa2b9999ac6f64e4de6b
Elouahmani <younes.elouahmani@gmail.com>
2021-10-04 22:22:32 +0200

    Use artemis docker instance
    to see dashboard of artemis

--- src/main/java/com/example/testspringjms/TestSpringJmsApplication.java
+++ src/main/java/com/example/testspringjms/TestSpringJmsApplication.java
@@ -1,21 +1,12 @@
 package com.example.testspringjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestSpringJmsApplication {
 
-    public static void main(String[] args) throws Exception {
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));
-        server.start();
+    public static void main(String[] args) {
         SpringApplication.run(TestSpringJmsApplication.class, args);
     }
 
