d06ffb887d800d05e1442c483c2bfdd05a9db6b6
Elouahmani <younes.elouahmani@gmail.com>
2021-10-04 21:02:27 +0200

    Send messages with activeMQ
    using jmsTemplate

---
+++ src/main/java/com/example/testspringjms/sender/HelloSender.java
@@ -0,0 +1,29 @@
+package com.example.testspringjms.sender;
+
+import com.example.testspringjms.config.JmsConfig;
+import com.example.testspringjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class HelloSender {
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        System.out.println("I'm sending a message");
+        HelloWorldMessage helloWorldMessage = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Hello World!")
+                .build();
+
+        jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, helloWorldMessage);
+
+        System.out.println("Message sent!");
+    }
+}
